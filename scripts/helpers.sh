e6ba0c1a3e0f7bf9626e0b0d8b5f14d57f895b2b
Brian Cosgrove <cosgroveb@gmail.com>
2025-07-08 10:58:03 -0500

    Remove unused get_last_sync_timestamp function

--- scripts/helpers.sh
+++ scripts/helpers.sh
@@ -16,14 +16,3 @@ get_last_sync_status() {
     echo "$stored_status"
 }
 
-get_last_sync_timestamp() {
-    local session="$1"
-    local namespace
-    namespace=$(tmux show-option -t "$session" -v "@buffer-sync-namespace" 2>/dev/null || echo "tmux-buffers")
-    [ -n "$namespace" ] || namespace="tmux-buffers"
-
-    local stored_timestamp
-    stored_timestamp=$(atuin kv get --namespace "$namespace" "_sync_timestamp_${session}" 2>/dev/null || date +%s)
-
-    echo "$stored_timestamp"
-}
